0fb391f809304fe261da0836b9f40641995838b5
Matteo <metboolean@gmail.com>
2021-01-15 15:20:37 +0100

    preparazione

---
+++ js/script.js
@@ -0,0 +1,3 @@
+/*Un alert espone 5 numeri casuali diversi.
+Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.*/
